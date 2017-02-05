fd8ba4f283a480a993a8cf25d5852b7607331581
HAMADA Takayuki <hamada.takayuki@gmail.com>
2017-02-06 01:32:02 +0900

    quiz controller view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
   get 'hello_world', to: 'hello_world#index'
+
+  root 'index#index'
   resources :questions
+
+  namespace :quiz do
+    resources :finished, only: [:index] 
+  end
   resources :quiz, only: [:show, :update]
 
 end
