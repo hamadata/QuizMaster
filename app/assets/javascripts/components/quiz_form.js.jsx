a9b2e983a1b19c8073d754a5b1bc8efd8c1c8f2d
HAMADA Takayuki <hamada.takayuki@gmail.com>
2017-02-08 13:46:31 +0900

    wip quiz_form.jsx

--- app/assets/javascripts/components/quiz_form.js.jsx
+++ app/assets/javascripts/components/quiz_form.js.jsx
@@ -6,12 +6,29 @@ var QuizForm = React.createClass({
         });
     },
 
+    handleSubmit: function(event) {
+        event.preventDefault();
+        var answer =
+        $.ajax({
+            url: "id",
+            dataType: 'json',
+            type: 'POST',
+            data: answer,
+            success: function(data) {
+                this.setState({data: data})
+            },
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
     render: function () {
         return (
             <div>
                 <h1>Quiz</h1>
                 <div className="content" dangerouslySetInnerHTML={{__html: this.state.body}}></div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label>Answer</label>
                         <input type="textarea" className="form-control" placeholder="write answer here" />
