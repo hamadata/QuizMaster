dc4e7d729125017caa4d1842332695a5dd75b63d
HAMADA Takayuki <hamada.takayuki@gmail.com>
2017-02-08 18:07:19 +0900

    (master commit) add questions seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Question.create({body: "<p>Which is the invention of Oliver Heaviside?</p><ol> <li>mathematical techniques for the solution of differential equations</li><li>gasoline engine</li><li>steam locomotives</li> </ol>", answer: "one"})
 Question.create({body: "<p>If it is 1:00 in Greenwitch Mean Time, how many o'clock in Japan?</p><p>Please answer in format of * o'clock.</p>", answer: "ten o'clock"})
 Question.create({body: "<p>If it is 8:00 in Manila, how many o'clock in Japan?</p><p>Please answer in format of * o'clock.</p>", answer: "9 o'clock"})
 Question.create({body: "<p>If it is 8:00 in Japan, how many o'clock in Jakarta?</p><p>Please answer in format of * o'clock.</p>", answer: "six o'clock"})
