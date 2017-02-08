ce494b07ddaed58977c38c8f8b5d87c0f6dc6b23
HAMADA Takayuki <hamada.takayuki@gmail.com>
2017-02-08 17:54:01 +0900

    (master commit) add some questions in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create({body: "<p>If it is 1:00 in Greenwitch Mean Time, how many o'clock in Japan?</p><p>Please answer in format of * o'clock.</p>", answer: "ten o'clock"})
+Question.create({body: "<p>If it is 8:00 in Manila, how many o'clock in Japan?</p><p>Please answer in format of * o'clock.</p>", answer: "9 o'clock"})
+Question.create({body: "<p>If it is 8:00 in Japan, how many o'clock in Jakarta?</p><p>Please answer in format of * o'clock.</p>", answer: "six o'clock"})
+Question.create({body: "<p>If it is 1:00 in Mexico City, how many o'clock in London?</p><p>Please answer in format of * o'clock.</p>", answer: "seven o'clock"})
